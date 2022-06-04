7208fbc4f8687cb5884edc83d90b1d349f1aeec5
bluejam100 <88756567+bluejam100@users.noreply.github.com>
2022-06-04 04:23:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,133 @@
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className += " nav-d";
+  } else {
+    x.className = "";
+  }
+
+
+var x = document.getElementById("sticky");
+  if (x.className === "sticky") {
+    x.className += " stickyd";
+  } else {
+    x.className = "sticky";
+  }
+
+var x = document.getElementById("list-inline");
+  if (x.className === "list-inline") {
+    x.className += " list-inline-d";
+  } else {
+    x.className = "list-inline";
+  }
+
+var x = document.getElementsByClassName("stickA");
+
+    
+for (var i = 0; i < x.length; i++) { 
+    
+  if (x[i].className === "stickA") {
+    x[i].className += " stickA-d";
+  } else {
+    x[i].className = "stickA";
+  }
+}
+
+
+var x = document.getElementsByClassName("medium-box");
+for (var i = 0; i < x.length; i++) { 
+    if (x[i].className === "medium-box") {
+    x[i].className += " medium-box-d";
+  } else {
+    x[i].className = "medium-box";
+  }
+}
+
+}
+
+
+
+
+
+//Header//
+
+class MyHeader extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+ 
+ <div class="image-container">
+    <img class="maid" src="maid_logo.png" alt="maid header">
+ </div>
+
+      <header id="sticky" class="sticky"> 
+
+<div class="topnav" id="myTopnav">
+       
+<button type="button" class="menu-toggle" onclick="myFunction()" >
+           <i class="fa fa-bars"></i> 
+    Menu 
+</button>
+   
+    </div>
+  
+    
+  <nav id="nav">
+    <ul id="list-inline" class="list-inline"> 
+      <li class="medium-box"><a class="stickA" href="index.html">Home Page</a></li>
+      <li class="medium-box"><a class="stickA" href="art.html">Art</a></li>
+      <li class="medium-box"><a class="stickA" href="newsletter.html">Newsletters</a></li>
+      <li class="medium-box"><a class="stickA" href="contact.html">Contact Us</a></li>
+      <li class="medium-box"><a class="stickA" href="faq.html">FAQ</a></li>
+    </ul>
+  </nav>
+    
+</div> 
+
+</header>
+       ` 
+  }
+}
+
+customElements.define('my-header',MyHeader)
+
+//Footer//
+
+class MyFooter extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+      
+  <footer>
+ 
+    <div class="first-box">
+    <div class="smicons"><img class="discord" src="Discord_logo.png" alt="discord logo"> 
+    </div>
+    
+    <div class="smicons"><img class="facebook" src="facebook_logo_3.png" alt="facebook logo">
+    </div>
+
+    <div class="smicons">
+      <img class="email" src="email_logo.png" alt="email logo">
+    </div>
+
+    </div>
+<hr>
+    <!--
+<div class="second-box">
+    <div class="small-box">
+      <p>
+       <a href=#> Top page</a>
+      </p>
+    </div>
+</div>
+      -->
+
+<div class= "third-box">
+   <p class="made-with-love"> Made with 🤎 Christina, advised by Abu, and art by Song </p>
+</div>
+    
+  </footer>
+       ` 
+  }
+}
+
+customElements.define('my-footer',MyFooter)
